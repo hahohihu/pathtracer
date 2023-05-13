913ee4f144589a229afa159ff829751177c7b749
hahohihu <pshen4@gmail.com>
2023-05-13 13:47:18 -0700

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    for iy in (0..image_height).rev() {
+        for ix in 0..image_width {
+            let r = ix as f64 / (image_width as f64 - 1.0);
+            let g = iy as f64 / (image_height as f64 - 1.0);
+            let b = 0.25;
+
+            let ir = (r * 255.999) as i64;
+            let ig = (g * 255.999) as i64;
+            let ib = (b * 255.999) as i64;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
