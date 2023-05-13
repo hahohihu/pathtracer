b616747f1f366612c3fb4656833ab5508cbb867c
hahohihu <pshen4@gmail.com>
2023-05-13 14:07:29 -0700

    nicer output

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::{io::{Write, stdout, BufWriter}, fs::File};
+
+const RESET_LINE: &str = "\x1B[2K\r"; 
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
+    let output = File::create("image.ppm").unwrap();
+    let mut out = BufWriter::new(output);
 
-    println!("P3");
-    println!("{image_width} {image_height}");
-    println!("255");
+    writeln!(out, "P3").unwrap();
+    writeln!(out, "{image_width} {image_height}").unwrap();
+    writeln!(out, "255").unwrap();
 
     for iy in (0..image_height).rev() {
+        print!("{RESET_LINE}");
+        print!("Scanlines remaining: {iy}");
+        stdout().flush().unwrap();
         for ix in 0..image_width {
             let r = ix as f64 / (image_width as f64 - 1.0);
             let g = iy as f64 / (image_height as f64 - 1.0);
@@ -16,7 +25,9 @@ fn main() {
             let ig = (g * 255.999) as i64;
             let ib = (b * 255.999) as i64;
 
-            println!("{ir} {ig} {ib}");
+            writeln!(out, "{ir} {ig} {ib}").unwrap();
         }
     }
+    print!("{RESET_LINE}");
+    println!("Done.");
 }
