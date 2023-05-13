445c2cae6d5e927a5d63f390d0bbc176c0657252
hahohihu <pshen4@gmail.com>
2023-05-13 16:18:18 -0700

    simple background

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,50 @@ mod vec3;
 mod ray;
 
 use std::{io::{Write, stdout, BufWriter}, fs::File};
+use ray::Ray;
 use vec3::*;
 
 const RESET_LINE: &str = "\x1B[2K\r"; 
 
+fn ray_color(ray: &Ray) -> Color {
+    let unit_dir = ray.direction.unit_vec();
+    let t = 0.5 * (unit_dir.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
     let output = File::create("image.ppm").unwrap();
     let mut out = BufWriter::new(output);
+
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400.0;
+    let image_height = image_width / aspect_ratio;
 
     writeln!(out, "P3").unwrap();
     writeln!(out, "{image_width} {image_height}").unwrap();
     writeln!(out, "255").unwrap();
 
-    for iy in (0..image_height).rev() {
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    // Render
+    for iy in (0..image_height as i64).rev() {
         print!("{RESET_LINE}");
         print!("Scanlines remaining: {iy}");
         stdout().flush().unwrap();
-        for ix in 0..image_width {
-            let color = Color::new(ix as f64 / (image_width as f64 - 1.0), iy as f64 / (image_height as f64 - 1.0), 0.25);
+        for ix in 0..image_width as i64 {
+            let u = (ix as f64) / (image_width - 1.0);
+            let v = (iy as f64) / (image_height - 1.0);
+            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            let color = ray_color(&ray);
             color.show_as_color(&mut out);
         }
     }
