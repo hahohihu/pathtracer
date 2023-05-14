9618f213469ca94f2122571ebc4b9effd943b0d4
hahohihu <pshen4@gmail.com>
2023-05-14 12:20:25 -0700

    hollow glass sphere

--- src/main.rs
+++ src/main.rs
@@ -91,17 +91,22 @@ fn main() {
     world.add(Rc::new(Sphere::new(
         Point::new(0.0, 0.0, -1.0),
         0.5,
-        glass,
+        matte,
     )));
     world.add(Rc::new(Sphere::new(
         Point::new(-1.0, 0.0, -1.0),
         0.5,
-        metal1,
+        glass.clone(),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point::new(-1.0, 0.0, -1.0),
+        -0.4,
+        glass,
     )));
     world.add(Rc::new(Sphere::new(
         Point::new(1.0, 0.0, -1.0),
         0.5,
-        matte,
+        metal2,
     )));
     // world.add(Rc::new(Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0)));
 
